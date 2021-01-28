2ba0cb40a34e60c105079d9dbfdddd5c2f360457
Ayush Gemini <ayushgemini13@gmail.com>
2021-01-28 13:22:41 +0530

    Added - Timber all types of logs

--- app/src/main/java/com/example/timber_logging/MainActivity.java
+++ app/src/main/java/com/example/timber_logging/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import timber.log.Timber;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Timber.d("debug message");
+        Timber.e("error message");
+        Timber.tag("Use any tag").d("add any debug message");
+        Timber.tag("Use any tag").e("add any error message");
     }
 }
